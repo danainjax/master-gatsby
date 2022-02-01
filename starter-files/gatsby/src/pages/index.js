331948a4e125650aaa800ebb8707424694811d0c
danainjax <danainjax@gmail.com>
2022-02-01 07:18:40 -0500

    added gatsby-browser to wrap all the pages in the layout component

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Layout from '../components/Layout';
 
-export default function HomePage({ children }) {
+export default function HomePage() {
   return (
-    <Layout>
-      {children}
-      <p>Homey page</p>
-    </Layout>
+    <>
+      <p>Home page</p>
+    </>
   );
 }
