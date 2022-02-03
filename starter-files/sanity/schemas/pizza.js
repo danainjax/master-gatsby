ba4cc74bae0945988a5c5f86ffa44c2399d716e2
danainjax <danainjax@gmail.com>
2022-02-03 05:23:48 -0500

    created headless cms

---
+++ starter-files/sanity/schemas/pizza.js
@@ -0,0 +1,25 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  name: 'pizza',
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+  ],
+};
